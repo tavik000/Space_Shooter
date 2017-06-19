86a9f1918bc34f90dcfe91a230210162ee541ace
tavik000 <tavik002@gmail.com>
2017-06-19 16:05:25 +0800

    Initialization

---
+++ Assets/Scripts/AsteroidMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidMover : MonoBehaviour
+{
+
+    public float speed;
+    public float speedRange;
+
+    void Start()
+    {
+        this.GetComponent<Rigidbody>().velocity = transform.forward * (Random.Range (speed - speedRange, speed + speedRange));
+
+    }
+}
+
---
+++ Assets/Scripts/DestoryByTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float LifeTime;
+    
+	void Start ()
+    {
+        Destroy(gameObject, LifeTime);
+    }
+	
+}
