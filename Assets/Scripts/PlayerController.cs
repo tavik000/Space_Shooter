0e5edd10a48cbc7f361da4fd17bc9ed45dee38ee
tavik000 <tavik002@gmail.com>
2017-07-28 10:28:23 +0800

    Mobile ver0.1
    nothing speicial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,7 +26,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
-            foreach(var shotSpawn in shotSpawns)
+            foreach (var shotSpawn in shotSpawns)
             {
                 Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
             }
@@ -36,18 +36,23 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-       
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        Vector3 acceleration = Input.acceleration;
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        this.GetComponent<Rigidbody>().velocity=  movement * speed;
+
+        this.GetComponent<Rigidbody>().velocity = movement * speed;
 
         this.GetComponent<Rigidbody>().position = new Vector3
         (
-            Mathf.Clamp(this.GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+            Mathf.Clamp(this.GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
             0.0f,
-            Mathf.Clamp(this.GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)           
+            Mathf.Clamp(this.GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
         );
 
         this.GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, this.GetComponent<Rigidbody>().velocity.x * -tilt);
